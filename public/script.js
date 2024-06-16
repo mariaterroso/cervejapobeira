549e8b6f6ca8e18548a5795f689a1d87c7beffa9
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2024-06-16 18:47:42 +0100

    navbar color changed

---
+++ public/script.js
@@ -0,0 +1,7 @@
+document.getElementById('upArrow').addEventListener('click', function() {
+    alert('Up arrow clicked');
+});
+
+document.getElementById('downArrow').addEventListener('click', function() {
+    alert('Down arrow clicked');
+});
