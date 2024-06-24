74ff66eba8c3cd616ee1cff80eb9eaf9debc9023
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2024-06-24 23:17:20 +0100

    fix onde

--- public/onde-script.js
+++ public/onde-script.js
@@ -2,9 +2,9 @@ function toggle() {
     var radio1 = document.getElementById('radio1');
     var radio2 = document.getElementById('radio2');
     var radio3 = document.getElementById('radio3');
-    var div1 = document.getElementById('image-container1');
-    var div2 = document.getElementById('image-container2');
-    var div3 = document.getElementById('image-container3');
+    var div1 = document.getElementById('image-container11');
+    var div2 = document.getElementById('image-container22');
+    var div3 = document.getElementById('image-container33');
     
 
     if (radio1.checked) {
